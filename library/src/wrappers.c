873ac1084b0fc2ee9f78556b9e9491b988f61290
salTermine <salvatore.termine@stonybrook.edu>
2017-05-05 20:38:15 -0400

    More wrappers

--- library/src/wrappers.c
+++ library/src/wrappers.c
@@ -107,6 +107,67 @@ wrapper(int, fchownat, int dirfd, const char *pathname, uid_t owner, gid_t group
 }
 
 /**
+ * Basic wrapper for fstat(2). It logs the fstat request, then invokes
+ * glibc fstat(2) with the given argument.
+ *
+ * Note that the function prototype for fstat(2) is defined in <unistd.h> <sys/types.h> <sys/stat.h>.
+ */
+
+wrapper(int, fstat, int fd, struct stat *statbuf) {
+
+	logmsg("Intercepted fstat call with fd: %d\n", fd);
+
+	_fstat = dlsym(RTLD_NEXT, "fstat");
+	return _fstat(fd, statbuf);
+}
+
+/**
+ * Basic wrapper for fstatat(2). It logs the fstatat request, then invokes
+ * glibc fstatat(2) with the given argument.
+ *
+ * Note that the function prototype for fstatat(2) is defined in <fnctl.h> <sys/stat.h>.
+ */
+
+wrapper(int, fstatat, int dirfd, const char *pathname, struct stat *statbuf, int flags) {
+
+	logmsg("Intercepted fstatat call with dirfd: %d, path: %s, flags: %d\n", dirfd, pathname, flags);
+
+	_fstatat = dlsym(RTLD_NEXT, "fstatat");
+	return _fstatat(dirfd, pathname, statbuf, flags);
+}
+
+/**
+ * Basic wrapper for fstatfs(2). It logs the fstatfs request, then invokes 
+ * glibc fstatfs(2) with the given argument.
+ *
+ * Note that the function prototype for fstatfs(2) is defined in <sys/vfs.h>
+ */
+wrapper(int, fstatfs, int fd, struct statfs *buf) {
+
+	logmsg("Intercepted fstatfs call with fd: %d\n", fd);
+
+	_fstatfs = dlsym(RTLD_NEXT, "fstatfs");
+	return _fstatfs(fd, buf);
+}
+
+/**
+ * Basic wrapper for lchown(2). It logs the lchown request, then invokes
+ * glibc lchown(2) with the given argument.
+ *
+ * Note that the function prototype for lchown(2) is defined in <unistd.h>.
+ */
+
+wrapper(int, lchown, const char *pathname, uid_t owner, gid_t group) {
+
+	logmsg("Intercepted lchown call with path: %s, uid: %lu, gid: %lu\n", pathname, owner, group);
+
+	_lchown = dlsym(RTLD_NEXT, "lchown");
+	return _lchown(pathname, owner, group);
+}
+
+
+
+/**
  * Basic wrapper for link(2). It logs the link request, then invokes 
  * glibc link(2) with the given argument.
  *
@@ -134,6 +195,22 @@ wrapper(int, linkat, int olddirfd, const char *oldpath, int newdirfd, const char
 	_linkat = dlsym(RTLD_NEXT, "linkat");
 	return _linkat(olddirfd,oldpath, newdirfd, newpath);
 }
+
+/**
+ * Basic wrapper for lstat(2). It logs the lstat request, then invokes 
+ * glibc lstat(2) with the given argument.
+ *
+ * Note that the function prototype for lstat(2) is defined in <sys/types.h> <sys/stat.h> <unistd.h>
+ */
+wrapper(int, lstat, const char *pathname, struct stat *statbuf) {
+
+	logmsg("Intercepted lstat call with path: %s\n", pathname);
+
+	_lstat = dlsym(RTLD_NEXT, "lstat");
+	return _lstat(pathname, statbuf);
+}
+
+
 
 /**
  * Basic wrapper for mkdir(2). It logs the mkdir request, then invokes
@@ -272,6 +349,54 @@ wrapper(ssize_t, readlinkat, int dirfd, const char *pathname, char *buf, size_t 
 }
 
 /**
+ * Basic wrapper for rename(2). It logs the rename(2) request, then invokes
+ * glibc rename with the given argument.
+ *
+ * Note that the function prototype for rename is defined in <stdio.h>.
+ */
+
+wrapper(int, rename, const char *oldpath, const char *newpath) {
+
+    logmsg("Intercepted rename call with oldpath: %s, newpath: %s\n", oldpath, newpath);
+
+    _rename = dlsym(RTLD_NEXT, "rename");
+    return _rename(oldpath, newpath);
+}
+
+/**
+ * Basic wrapper for renameat(2). It logs the renameat(2) request, then invokes
+ * glibc renameat with the given argument.
+ *
+ * Note that the function prototype for renameat is defined in <fcntl.h> <stdio.h>.
+ */
+
+wrapper(int, renameat, int olddirfd, const char *oldpath, int newdirfd, const char *newpath) {
+
+    logmsg("Intercepted renameat call with olddirfd: %s, oldpath: %s, newdirfd: %d\n", olddirfd, oldpath, newpath);
+
+    _renameat = dlsym(RTLD_NEXT, "renameat");
+    return _renameat(olddirfd, oldpath, newpath);
+}
+
+/**
+ * Basic wrapper for renameat2(2). It logs the renameat2(2) request, then invokes
+ * glibc renameat2 with the given argument.
+ *
+ * Note that the function prototype for renameat2 is defined in <fcntl.h> <stdio.h>.
+ */
+
+wrapper(int, renameat2, int olddirfd, const char *oldpath, int newdirfd, const char *newpath, unsigned int flags) {
+
+    logmsg("Intercepted renameatat2 call with olddirfd: %s, oldpath: %s, newdirfd: %d, newpath: %s, flags: %d\n", 
+    	olddirfd, oldpath, newdirfd, newpath, flags);
+
+    _renameat2 = dlsym(RTLD_NEXT, "renameat2");
+    return _renameat2(olddirfd, oldpath, newdirfd, newpath, flags);
+}
+
+
+
+/**
  * Basic wrapper for rmdir. It logs the rmdir request, then invokes
  * glibc rmdirt with the given argument.
  *
@@ -283,6 +408,34 @@ wrapper(int, rmdir, const char *pathname) {
 
 	_rmdir = dlsym(RTLD_NEXT, "rmdir");
 	return _rmdir(pathname);
+}
+
+/**
+ * Basic wrapper for stat(2). It logs the stat request, then invokes 
+ * glibc stat(2) with the given argument.
+ *
+ * Note that the function prototype for stat(2) is defined in <sys/types.h> <sys/stat.h> <unistd.h>
+ */
+wrapper(int, stat, const char *pathname, struct stat *statbuf) {
+
+	logmsg("Intercepted stat call with path: %s\n", pathname);
+
+	_stat = dlsym(RTLD_NEXT, "stat");
+	return _stat(pathname, statbuf);
+}
+
+/**
+ * Basic wrapper for statfs(2). It logs the statfs request, then invokes 
+ * glibc statfs(2) with the given argument.
+ *
+ * Note that the function prototype for statfs(2) is defined in <sys/vfs.h>
+ */
+wrapper(int, statfs, const char *path, struct statfs *buf) {
+
+	logmsg("Intercepted statfs call with path: %s\n", pathname);
+
+	_statfs = dlsym(RTLD_NEXT, "statfs");
+	return _statfs(pathname, buf);
 }
 
 /**
