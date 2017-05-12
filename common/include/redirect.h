e4b3582e76351a939d4df532dca0ac6e5213dbac
salTermine <salvatore.termine@stonybrook.edu>
2017-05-12 15:31:16 -0400

    Redirect.h

---
+++ common/include/redirect.h
@@ -0,0 +1,10 @@
+#ifndef _REDIRECT_H
+#define _REDIRECT_H
+
+
+int sip_should_redirect(const char *pathname, int mode);
+int sip_is_redirect(const char *pathname);
+const char *sip_build_redirect_path(const char *pathname);
+const char *sip_revert_path(const char *pathname);
+
+#endif
