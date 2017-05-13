fee712e392a6814efb69713b7d34c452a6b2e6b7
Brett Porcelli <brettporcelli@gmail.com>
2017-05-12 19:36:33 -0700

    Refactor to avoid compiler warnings.

--- common/include/redirect.h
+++ common/include/redirect.h
@@ -1,11 +1,12 @@
 #ifndef _REDIRECT_H
 #define _REDIRECT_H
 
+#include <sys/types.h>
 
-int sip_should_redirect(const char *pathname, int mode);
-int sip_is_redirect(const char *pathname);
-const char *sip_build_redirect_path(const char *pathname);
-const char *sip_revert_path(const char *pathname);
-const char *sip_convert_if(const char *pathname);
+int sip_should_redirect(const char *pathname, mode_t mode);
+int sip_is_redirected(const char *pathname);
+char *sip_build_redirect_path(const char *pathname);
+char *sip_revert_path(char *pathname);
+char *sip_convert_to_redirected_path(char *pathname);
 
 #endif
