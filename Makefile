053f10cc3c28b87fee65b7aeee2dbdf6d3f7c9e2
Brett Porcelli <brettporcelli@gmail.com>
2017-05-15 07:55:52 -0700

    Remove PiP files.

--- Makefile
+++
@@ -1,4 +0,0 @@
-DIRS = objects include bin logs logs/error_logs logs/install_logs utilities
-
-clean:
-	rm -rf $(DIRS)
--- pip/lwip_installation/install_bsd.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo pw useradd -nuntrusted -u1004
-#sudo pw usermod ubuntu -G untrusted
-sudo pw useradd -u1005 -nuntrustedRoot 
-
-sudo pw groupadd -g1006 -ntrusted_group
-cat /etc/passwd|grep ":"|cut -f 1 -d ':'|grep -v untrusted|xargs -n1 -I'{}' bash -c "sudo pw usermod {} -G trusted_group"
